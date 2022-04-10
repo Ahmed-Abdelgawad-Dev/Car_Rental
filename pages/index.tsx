10f6426676db00d266cd37daf57e3a49621eb1a2
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-04-11 01:19:12 +0200

    Navbar ammendment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import {AppBar, Box, Typography, Toolbar, IconButton, Button} from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
-
+import {
+  AppBar,
+  Box,
+  Typography,
+  Toolbar,
+  IconButton,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Home: NextPage = () => {
   return (
@@ -17,25 +23,30 @@ const Home: NextPage = () => {
         />
       </Head>
       <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography color="yellow" variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Selmi Cars
-          </Typography>
-          <Button color="inherit">Login</Button>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color="yellow"
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              Selmi Cars
+            </Typography>
+            <Button color="inherit">Login</Button>
             <Button color="inherit">Register</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
   );
 };
