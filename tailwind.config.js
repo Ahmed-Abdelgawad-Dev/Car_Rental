759419cb6150ac20df4ae3968152d170e95e0300
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-04-13 05:03:58 +0200

    Tailwindcss configs to avoid bootstrap styling for using both without collisions

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,26 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+  prefix: "tw-",
+  important: true,
+  theme: {
+    // || Here sizes can be configuered down under screens to match ||
+    // || any css framework sizes(Here:  Bootstrap breakpoints - 1) ||
+    screens: {
+      sm: "575px",
+      md: "767px",
+      lg: "991px",
+      xl: "1199",
+      "2xl": "1399",
+    },
+    extend: {
+      colors: {
+        highlight: "#FF6B00",
+      },
+    },
+  },
 };
