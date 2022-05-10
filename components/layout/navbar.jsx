aa5b74cd199fc90084905a970850d5e19f040c74
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-05-10 06:36:18 +0200

    Nav bootstrap with tailwind

---
+++ components/layout/navbar.jsx
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Appnavbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light tw-bg-white ">
+        <div className="container-fluid">
+          <Link href="/">
+            <a>
+              <img
+                style={{ width: "100px" }}
+                layout="fill"
+                classNameName="tw-w-10 tw-object-contain tw-cursor-pointer"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi4bmuZOzNXLTohhEgEn3jvwyH_BTl4-s02_-eGyAfNu8QYdOHmzAHbZk5-k7QnmMYJPc&usqp=CAU"
+                //   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi4bmuZOzNXLTohhEgEn3jvwyH_BTl4-s02_-eGyAfNu8QYdOHmzAHbZk5-k7QnmMYJPc&usqp=CAU"
+                alt="Logo"
+              />
+            </a>
+          </Link>
+          <a className="tw-text-red-700 tw-font-semibold tw-uppercase" href="#">
+            Selmi cars
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto tw-space-x-10">
+              <li className="nav-item">
+                <a
+                  className="nav-link active tw-text-black-700 tw-font-semibold"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active tw-text-black tw-font-semibold "
+                  href="#"
+                >
+                  Car rental
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active tw-text-black-  tw-font-semibold"
+                  href="#"
+                >
+                  Contact us
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                className="tw-text-white tw-bg-red-700 tw-rounded-md tw-px-4 tw-p-2 tw-font-semibold hover:tw-bg-red-900"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Appnavbar;
