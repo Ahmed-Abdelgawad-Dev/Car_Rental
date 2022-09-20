a2d40d903b832fa90d17fea4a71d3a24d2a58c2b
ahmed-abdelgawad-dev <ahmed.abdelgawad.dev@gmail.com>
2022-09-20 18:26:15 +0200

    applied car model on schema

--- proj/core/models.py
+++ proj/core/models.py
@@ -1,1 +1,11 @@
+from cgi import print_exception
 from django.db import models
+from django.utils.text import format_lazy as _
+
+class Car(models.Model):
+    brand = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    year = models.CharField(max_length=255)
+    price = models.PositiveBigIntegerField()
+    color = models.CharField(max_length=255)
+    image = models.ImageField(_("image"), upload_to="images", height_field=None, width_field=None, max_length=None)
