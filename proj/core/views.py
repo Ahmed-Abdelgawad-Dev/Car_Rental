fb33fce14906b3275d34733277e9714257a298e1
ahmed-abdelgawad-dev <ahmed.abdelgawad.dev@gmail.com>
2022-11-06 17:58:36 +0400

    Typedjs added in landing

--- proj/core/views.py
+++ proj/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def frontend(request):
     return render(request, 'core/frontend.html')
+
+def cars(request):
+    return render(request, 'core/sections/cars.html')
--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import frontend
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', frontend, name='frontend'),
+    path('', include('core.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
